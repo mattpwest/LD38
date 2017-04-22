522f1f4963f54e47661202dc37fd80ce6735ebd6
Matt Van Der Westhuizen <matt.vanderwest@entelect.co.za>
2017-04-22 14:07:43 +0200

    Added an interface for a TileViewFactory.

---
+++ Core/Match3.Core.UI/Views/ITileViewFactory.cs
@@ -0,0 +1,9 @@
+﻿using Match3.Core.UI.Presenters;
+
+namespace Match3.Core.UI.Views {
+    interface ITileViewFactory
+    {
+        ITileView CreateInitial(IBoardPresenter presenter, string type, int x, int y);
+        ITileView Create(IBoardPresenter presenter, string type, int x, int y);
+    }
+}
