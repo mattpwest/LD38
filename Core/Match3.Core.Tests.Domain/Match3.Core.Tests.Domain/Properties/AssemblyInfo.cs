3f85e2bee595314b6e1e1ecfd616c7477a9c3b42
Matt Van Der Westhuizen <matt.vanderwest@entelect.co.za>
2017-04-22 08:29:42 +0200

    Initial commit.

---
+++ Core/Match3.Core.Tests.Domain/Match3.Core.Tests.Domain/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Match3.Core.Tests.Domain")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Match3.Core.Tests.Domain")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6f604bb1-761a-4323-968d-954404dd9b7b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
