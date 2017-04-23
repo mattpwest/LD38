3aab698a04e1391731897a2f0c846d3856a97cd8
Matt Van Der Westhuizen <matt.vanderwest@entelect.co.za>
2017-04-23 10:05:14 +0200

    Main menu is done for now... just need to add click trigger to load next.

---
+++ Unity/LD38/Assets/Scripts/Menus/SpinEarth.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinEarth : MonoBehaviour
+{
+
+    public float RotationRateInDegreesPerSecond = 10.0f;
+    private float yRotation = 0.0f;
+
+	void Start () {
+		
+	}
+	
+	void Update ()
+	{
+	    this.yRotation += Time.deltaTime * this.RotationRateInDegreesPerSecond;
+        gameObject.transform.rotation = Quaternion.Euler(new Vector3(0.0f, yRotation, 0.0f));
+	}
+}
