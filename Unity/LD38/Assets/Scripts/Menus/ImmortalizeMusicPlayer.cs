a92a078354989e2af6ba599698c590f9ecf6042a
Matt Van Der Westhuizen <matt.vanderwest@entelect.co.za>
2017-04-24 23:10:40 +0200

    Added some basic background music.

---
+++ Unity/LD38/Assets/Scripts/Menus/ImmortalizeMusicPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImmortalizeMusicPlayer : MonoBehaviour {
+
+	void Start () {
+		DontDestroyOnLoad(this);
+	}
+	
+	void Update () {
+		
+	}
+}
